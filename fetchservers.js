c5e825701f88b6896442aaf41c4ccb0857e985d1
TTFreya <142606253+TTFreya@users.noreply.github.com>
2024-04-18 12:14:36 -0500

    comm

--- fetchservers.js
+++ fetchservers.js
@@ -2,9 +2,10 @@
 // Broadcasts every non-purchased server we can find to port[2]
 export async function main(ns) {
     await ns.getPortHandle(2); // This is the port that broadcasts the known server array to other documents. Used by: primer.js.
-    const knownservers = ['home']; // Shoutout to Cald for the help with fixing an bad loop here!
+    let knownservers = ['home']; // Shoutout to Cald for the help with fixing a bad loop here!
     for (let i = 0; i < knownservers.length; i++) {
         knownservers.push(...ns.scan(knownservers[i]).filter(server => !knownservers.includes(server) && !knownservers.includes("RAM")));
     } // The above script needs to be added onto to include optimal targeting when possible, but that isn't the priority right now.
     await ns.writePort(2, knownservers);
+    await ns.exec("primer.js", "home");
 }
--- primer.js
+++ primer.js
@@ -2,8 +2,32 @@
 // Primes our found servers for our hwgw batch to work.
 // Prime a server, then push the name of the server to port[3].
 export async function main(ns) {
-    servers = ns.getPortHandle(2); // This is the port that connects to fetchservers.js. Not sure if this line is needed.
-    for (let i = 0; i < servers.length; i++) {
-        
+    await ns.getPortHandle(2);
+    let server = ns.readPort(2); // This is the port that connects to fetchservers.js. Not sure if this line is needed.
+    let prog = [
+        BruteSSH,
+        FTPCrack,
+        HTTPWorm,
+        SQLInject,
+        RelaySMTP,
+        NUKE
+    ];
+    let progfunc = [
+        ns.brutessh(),
+        ns.ftpcrack(),
+        ns.httpworm(),
+        ns.sqlinject(),
+        ns.relaysmtp(),
+        ns.nuke()
+    ];
+    
+    for (let i = 0; i < server.length; i++) {
+        for (let j = 0; j < prog.length; j++) {
+            if (ns.fileExists(prog[j] + ".exe")) {
+                progfunc[j];
+            }
+        }
+        ns.writePort(3, server[i]);
+        ns.exec("hwgw-batch.js", "home");
     }
 }
