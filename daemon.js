6b6566bf2bfa0e4ba0e3218e39a54500700d8624
TTFreya <142606253+TTFreya@users.noreply.github.com>
2024-04-18 09:50:24 -0500

    Finished basic HWGW batch
    No server code yet

---
+++ daemon.js
@@ -0,0 +1,16 @@
+/** @param {NS} ns */
+// Write a script that consistently deploys a HWGW batch such that each job finishes in the correct order and within 20ms of each other and successfully returns the server to a prepped state.
+
+export async function main(ns) {
+    ns.getPortHandle(1); // Not sure if this is needed
+    ns.clearPort(1);
+    if ( ns.readPort(1) == "2"); { // There's probably a better way to handle this
+        ns.clearPort(1);
+    }
+    if ( ns.readPort(1) == "NULL PORT DATA");
+      // Import server code here
+      ns.writePort(1, "1") // Not sure if this is needed, just want to avoid an infinite loop.
+      ns.exec("hwgw-batch.js", "home");
+  
+  
+  }
---
+++ hwgw-batch.js
@@ -0,0 +1,23 @@
+/** @param {NS} ns */
+// Write a script that consistently deploys a HWGW batch such that each job finishes in the correct order and within 20ms of each other and successfully returns the server to a prepped state.
+// Weaken: 0
+// Weaken II: 20
+// Grow: weaktime - growtime + 20
+// Hack: growtime - hacktime + 20
+
+export async function main(ns) {
+    ns.getPortHandle(1); // Not sure if this is needed
+    let hacktime = ns.getHackTime(); // PH
+    let growtime = hacktime * 3.2;
+    let weaktime = hacktime * 4;
+  
+    ns.weaken(); // PH
+    await ns.sleep(20);
+    ns.weaken(); // PH
+    await ns.sleep(weaktime - growtime + 20);
+    ns.grow(); // PH
+    await ns.sleep(growtime - hacktime + 20);
+    await ns.hack(); // PH
+  
+    ns.writePort(1, "2");
+  }
