2499f82e2619421b9b226732f3c5b2e7cd8f8b37
TTFreya <142606253+TTFreya@users.noreply.github.com>
2024-04-24 08:34:26 -0500

    Update download.js

--- download.js
+++ download.js
@@ -20,10 +20,10 @@ export async function main(ns) {
     ns.tprint('='.repeat(20));
     
     if (filesImported) {
-        ns.tprint('Hey! Thank you for downloading the BitBurner Scripts.');
+        ns.tprint('Scripts have been downloaded.');
         ns.tprint(`You've installed these in the ${folder} directory.`);
-        ns.tprint(`A good place to start is running \`run /${folder}/hax.js\``);
+        ns.tprint(`\`Run /${folder}/daemon.js\``);
     } else {
-        ns.tprint('You had some issues downloading files, please reach out to the repo maintainer or check your config.');
+        ns.tprint('File download failed.');
     }
 }
