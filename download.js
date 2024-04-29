5aed1d0671eddca59323551d2b6bb550d1215c04
2TFreya <167789301+2TFreya@users.noreply.github.com>
2024-04-29 10:27:58 -0500

    Update download.js

--- download.js
+++ download.js
@@ -1,19 +1,19 @@
 export async function main(ns) {
+  const gray = '\x1b[37m'
   const { rootUrl } = ns.flags([['rootUrl', 'https://raw.githubusercontent.com/TTFreya/bitburner/main/']]);
   const folder = 'scripts';
   const files = [
     "fetchservers.js",
     "primer.js",
     "hwgw.js",
-    "daemon.js"
-    ];
+    "daemon.js"];
 
   let filesImported = true;
   for (let file of files) {
     const remoteFileName = `${rootUrl}scripts/${file}?t=${Date.now()}`;
     const result = await ns.wget(remoteFileName, `/${folder}/${file}`);
     filesImported = filesImported && result;
-    ns.tprint(`File: ${file}: ${result ? '✔️' : '❌'}`);
+    ns.tprint(`${gray}File: ${file}: ${result ? '✔️' : '❌'}${reset}`);
   }
 
   ns.tprint('='.repeat(20));
