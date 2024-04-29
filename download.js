343752007d6c9d239b246be4c1a9402bf8727244
2TFreya <167789301+2TFreya@users.noreply.github.com>
2024-04-29 10:17:14 -0500

    Update download.js

--- download.js
+++ download.js
@@ -1,29 +1,24 @@
 export async function main(ns) {
-    const { rootUrl } = ns.flags([['rootUrl', 'https://raw.githubusercontent.com/TTFreya/bitburner/main/']]); 
-    const folder = 'scripts';
-
-    const files = [
-        "fetchservers.js",
-        "primer.js",
-        "hwgw.js",
-        "daemon.js"
-    ];
-    
-    let filesImported = true;
-    for (let file of files) {
-        const remoteFileName = `${rootUrl}scripts/${file}?t=${Date.now()}`;
-        const result = await ns.wget(remoteFileName, `/${folder}/${file}`);
-        filesImported = filesImported && result;
-        ns.tprint(`File: ${file}: ${result ? '✔️' : '❌'}`);
-    }
-
-    ns.tprint('='.repeat(20));
-    
-    if (filesImported) {
-        ns.tprint('SUCCESS: Scripts have been downloaded.');
-        ns.tprint(`INFO: You've installed these in the ${folder} directory.`);
-        ns.tprint(`INFO: \`Run /${folder}/daemon.js\``);
-    } else {
-        ns.tprint('ERROR: File download failed.');
-    }
+  const { rootUrl } = ns.flags([['rootUrl', 'https://raw.githubusercontent.com/TTFreya/bitburner/main/']]);
+  const folder = 'scripts';
+  const files = [
+    "fetchservers.js",
+    "primer.js",
+    "hwgw.js",
+    "daemon.js"];
+  let filesImported = true;
+  for (let file of files) {
+    const remoteFileName = `${rootUrl}scripts/${file}?t=${Date.now()}`;
+    const result = await ns.wget(remoteFileName, `/${folder}/${file}`);
+    filesImported = filesImported && result;
+    ns.tprint(`File: ${file}: ${result ? '✔️' : '❌'}`);
+  }
+  ns.tprint('='.repeat(20));
+  if (filesImported) {
+    ns.tprint('SUCCESS: Scripts have been downloaded.');
+    ns.tprint(`INFO: You've installed these in the ${folder} directory.`);
+    ns.tprint(`INFO: \`Run /${folder}/daemon.js\``); // Fix a text error here
+  } else {
+    ns.tprint('ERROR:File download failed.');
+  }
 }
