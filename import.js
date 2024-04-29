a467c4f036cc2f6e7c13959df7289d30290eb2ff
2TFreya <167789301+2TFreya@users.noreply.github.com>
2024-04-29 10:21:28 -0500

    Update import.js

--- import.js
+++ import.js
@@ -1,6 +1,29 @@
 export async function main(ns) {
-    const rootUrl = 'https://raw.githubusercontent.com/TTFreya/bitburner/main/';
-    const downloadFileName = `${rootUrl}download.js?t=${Date.now()}`;
-    await ns.wget(downloadFileName, `download.js`);
-    ns.run(`download.js`, 1, `--rootUrl=${rootUrl}`);
+  const folder = 'scripts';
+  const files = [
+    "fetchservers.js", // Throwing an error with assertion? Sometimes?
+    "primer.js",
+    "hwgw.js",
+    "daemon.js"];
+  let filesDeleted = true
+  for (let file of files) {
+    const removee = `scripts/${file}`
+    const result = await ns.rm(removee);
+    filesDeleted = filesDeleted && result
+    // Update this to check for only outdated files rather than axing everything in /scripts/*
+    ns.tprint(`Clearing Outdated File: ${file}: ${result ? '✔️' : '❌'}`);
   }
+
+  if (filesDeleted) {
+    ns.tprint('SUCCESS: Old scripts have been cleared successfully.');
+    ns.tprint('INFO: Attempting to download updated files from the Git Repo.');
+  } else {
+    ns.tprint('ERROR: File clearing failed. Debug import.js');
+  }
+
+  ns.tprint('='.repeat(20));
+  const rootUrl = 'https://raw.githubusercontent.com/TTFreya/bitburner/main/';
+  const downloadFileName = `${rootUrl}download.js?t=${Date.now()}`;
+  await ns.wget(downloadFileName, `download.js`);
+  ns.run(`download.js`, 1, `--rootUrl=${rootUrl}`);
+}
