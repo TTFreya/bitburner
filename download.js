55d20a1ad91c1b52c73db3c5602e8decac8626ee
TTFreya <142606253+TTFreya@users.noreply.github.com>
2024-06-03 12:22:12 +0200

    prettification

--- download.js
+++ download.js
@@ -1,36 +1,38 @@
 export async function main(ns) {
-  const gray = '\x1b[37m'
-  const reset = "\u001b[0m"
-  const { rootUrl } = ns.flags([['rootUrl', 'https://raw.githubusercontent.com/TTFreya/bitburner/main/']]);
-  const folder = 'scripts';
-  const files = [
-    "fetchservers.js",
-    "primer.js",
-    "hwgw.js",
-    "daemon.js"];
-  const shutub = [
-    "wget",
-  ]
-
-  ns.tail(); 
-  for (let command of stfu) {
-    ns.disableLog(command)
+    const gray = '\x1b[37m'
+    const reset = "\u001b[0m"
+    const { rootUrl } = ns.flags([['rootUrl', 'https://raw.githubusercontent.com/TTFreya/bitburner/main/']]);
+    const folder = 'scripts';
+    const files = [
+      "fetchservers.js",
+      "primer.js",
+      "hwgw.js",
+      "daemon.js"];
+    const shutup = [
+      "wget",
+      "disableLog"
+    ];
+  
+    ns.tail(); 
+    for (let command of shutup) {
+      ns.disableLog(command);
+    }
+  
+    let filesImported = true;
+    for (let file of files) {
+      const remoteFileName = `${rootUrl}scripts/${file}?t=${Date.now()}`;
+      const result = await ns.wget(remoteFileName, `/${folder}/${file}`); // ty Hydrogenious in the Bitburner `cord for telling me how to shut up wget
+      filesImported = filesImported && result;
+      ns.print(`${gray}File: ${file}: ${result ? '✓' : '✗'}${reset}`);
+    }
+  
+    ns.print(`${gray}=`.repeat(50));
+    if (filesImported) {
+      ns.print('SUCCESS: Scripts have been downloaded.');
+      ns.print(`INFO: You've installed these in the ${folder} directory.`);
+      ns.print(`INFO: \'Run /${folder}/daemon.js\``); // Fix a text error here, replace with apostrophes at your own peril
+    } else {
+      ns.print('ERROR:File download failed.');
+    }
   }
   
-  let filesImported = true;
-  for (let file of files) {
-    const remoteFileName = `${rootUrl}scripts/${file}?t=${Date.now()}`;
-    const result = await ns.wget(remoteFileName, `/${folder}/${file}`); // ty Hydrogenious in the Bitburner `cord for telling me how to shut up wget
-    filesImported = filesImported && result;
-    ns.print(`${gray}File: ${file}: ${result ? '✓' : '✗'}${reset}`);
-  }
-
-  ns.print(`${gray}=`.repeat(50));
-  if (filesImported) {
-    ns.print('SUCCESS: Scripts have been downloaded.');
-    ns.print(`INFO: You've installed these in the ${folder} directory.`);
-    ns.print(`INFO: \'Run /${folder}/daemon.js\``); // Fix a text error here, replace with apostrophes at your own peril
-  } else {
-    ns.print('ERROR:File download failed.');
-  }
-}
