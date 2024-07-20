ae753065321211c6178efdcc51be32a811ce3289
TTFreya <142606253+TTFreya@users.noreply.github.com>
2024-07-20 12:56:43 -0500

    s

--- scripts/init/online-test.js
+++ scripts/init/online-test.js
@@ -1,5 +1,22 @@
 /** @param {NS} ns */
 export async function main(ns) {
     await ns.getPortHandle(4);
-    // TODO
+    const gray = '\x1b[37m'
+    const reset = '\u001b[0m'
+    const files = [
+        "test.txt"
+    ];
+    let online = true;
+    for (let file of files) {
+      const remoteFileName = `${rootUrl}${file}?t=${Date.now()}`;
+      const result = await ns.wget(remoteFileName, `/${folder}/${file}`);
+      online = online && result;
+      ns.print(`${gray}Online Test: ${result ? '✓' : '✗'}${reset}`);
+    }
+    if (online == true) {
+        ns.exec("scripts/init/online-fetching/online-import.js", "home");
+    } else {
+        ns.exec("scripts/init/offline-fetching/offline-import.js", "home");
+    }
+
 }
